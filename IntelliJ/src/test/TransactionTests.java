02960d56013acb38fbf8406e7175e9fce3a84fe1
Marc Donald <marcsdeveloperemail@gmail.com>
2018-03-12 12:51:41 +0000

    Second Transaction constructor test

--- IntelliJ/src/test/TransactionTests.java
+++ IntelliJ/src/test/TransactionTests.java
@@ -16,17 +16,34 @@ public class TransactionTests
     /** Runs all Transaction tests */
     private static void runTests()
     {
-        normalEntry();
+        normalEntryConstructor1();
+        normalEntryConstructor2();
     }
 
     /** Test for normal entry. Tests date reformatting and storage ability*/
-    private static void normalEntry()
+    private static void normalEntryConstructor1()
     {
+        System.out.println("Normal Entry Constructor 1");
+
         Transaction transaction = new Transaction("03/12/1998", 1, "Stephen", 250.50);
 
         test.ckEqualsI("Date: ", 19981203, transaction.getDate());
         test.ckEqualsI("Category: ", 1, transaction.getCategory());
         test.ckEqualsS("Recipient: ", "Stephen", transaction.getRecipient());
         test.ckEqualsD("Amount: ", 250.50, transaction.getAmount());
+        System.out.println("");
+    }
+
+    /** Test for normal entry using second constructor. Tests date storage for formatted date */
+    private static void normalEntryConstructor2()
+    {
+        System.out.println("Normal Entry Constructor 2");
+        Transaction transaction = new Transaction(1, "Stephen", 250.50, "19981203");
+
+        test.ckEqualsS("Date: ", "19981203", Integer.toString(transaction.getDate()));
+        test.ckEqualsI("Cateogory: ", 1, transaction.getCategory());
+        test.ckEqualsS("Recipient: ", "Stephen", transaction.getRecipient());
+        test.ckEqualsD("Amount: ", 250.50, transaction.getAmount());
+        System.out.print("");
     }
 }
